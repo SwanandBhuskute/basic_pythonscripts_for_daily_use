565a38e3658c294f69a9283ab22f25e728af8119
priddhesh <109747774+priddhesh@users.noreply.github.com>
2022-10-01 16:52:24 +0530

    Update textSummarizer.py

--- TextSummarizer/textSummarizer.py
+++ TextSummarizer/textSummarizer.py
@@ -1,15 +1,15 @@
 from transformers import pipeline
 import argparse
 
-def textSummarizer(text):
+def textSummary(text):
   summarization = pipeline("summarization")
   original_text = text
-  summary_text = summarization(original_text)[0]['summary_text']
-  print("Summary:", summary_text)
+  summaryText = summarization(original_text)[0]['summary_text']
+  print("Summary:", summaryText)
 
-if __name__ == "__main__":
+if _name_ == "_main_":
     parser = argparse.ArgumentParser()
     parser.add_argument("text",type = str)
 
     args = parser.parse_args()
-    textSummarizer(args.text)
+    textSummary(args.text)
